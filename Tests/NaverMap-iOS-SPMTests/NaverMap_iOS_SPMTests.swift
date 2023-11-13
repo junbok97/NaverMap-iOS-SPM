1f8391e89fe12948bfa430e28029200f5f60cf83
junbok97 <junbok97@gmail.com>
2023-11-13 16:43:33 +0900

    Initial Commit

---
+++ Tests/NaverMap-iOS-SPMTests/NaverMap_iOS_SPMTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import NaverMap_iOS_SPM
+
+final class NaverMap_iOS_SPMTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
