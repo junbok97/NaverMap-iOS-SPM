bbe2b785674a94943f5d9c794db582a06d901eb2
junbok97 <junbok97@gmail.com>
2023-11-13 17:02:40 +0900

    [feat] iOS v16

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NaverMap-iOS-SPM",
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "NaverMap-iOS-SPM",
